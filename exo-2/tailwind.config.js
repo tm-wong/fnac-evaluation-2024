d1b41bd036aa5d2ff3f97b317cc7797d53e01524
theo <thierry.wong@latelier.co>
2024-07-02 20:18:28 +0200

    main frames

---
+++ exo-2/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    fontFamily: {
+      'amaranth-regular': ['AmaranthRegular'],
+      'amaranth-bold': ['AmaranthBold'],
+    }
+  },
+  plugins: [],
+}
